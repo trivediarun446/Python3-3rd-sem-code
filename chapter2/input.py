c2a63d3a02113e15db20ac21678149936415e8ae
arun <trivediarun446@gmail.com>
2024-09-10 05:29:25 +0530

    10/09/2024

---
+++ chapter2/input.py
@@ -0,0 +1,7 @@
+a = input("enter the valur of a : ")
+b = input("enter the value of b : ")
+print(a+b) 
+
+a = int(input)("enter the value of a :")
+b=int (input)("enter the valur of b : ")
+print(a+b)
